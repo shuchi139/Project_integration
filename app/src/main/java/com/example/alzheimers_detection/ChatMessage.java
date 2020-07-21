84216b55bc15cd4e19ad9c65dbd5e2628fedd908
Vaib1012 <56437283+Vaib1012@users.noreply.github.com>
2020-07-21 23:13:00 +0530

    fluency stage integrated

---
+++ app/src/main/java/com/example/alzheimers_detection/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.alzheimers_detection;
+
+class ChatMessage {
+    private String content;
+
+    public ChatMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
+
